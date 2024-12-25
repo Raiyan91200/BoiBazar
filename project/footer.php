51ebf7f5fa72650e45a7f0a8b835e4dcb7804745
Raiyan Nasim <133204226+Raiyan91200@users.noreply.github.com>
2024-12-25 09:51:39 +0600

    all upload

---
+++ project/footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+<div class="box-container">
+   
+         <div class="box">
+            <h3>quick links</h3>
+            <a href="home.html">home</a>
+            <a href="about.html">about</a>
+            <a href="shop.html">shop</a>
+            <a href="contact.html">contact</a>
+         </div>
+   
+         <div class="box">
+            <h3>extra links</h3>
+            <a href="login.html">login</a>
+            <a href="register.html">register</a>
+            <a href="cart.html">cart</a>
+            <a href="orders.html">orders</a>
+         </div>
+   
+         <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +8801537204470 </p>
+            <p> <i class="fas fa-phone"></i> +8801846593095 </p>
+            <p> <i class="fas fa-envelope"></i> raiyannasim91200@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Mirpur Dhaka-1216</p>
+         </div>
+   
+         <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+         </div>
+   
+      </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Green University</span> </p>
+
+</section>
